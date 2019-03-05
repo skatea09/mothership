5dacccce263df1e589b0f71946892d24964103e2
Ammon <skatea09@aol.com>
2019-03-04 17:18:20 -0800

    adds tooltips

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,28 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
-import { fetchUser } from '../redux/actions';
+import Popup from 'reactjs-popup';
+import { fetchUser } from "../redux/actions";
 import store from "../redux/store";
 
 class App extends Component {
   componentDidMount() {
-    store.dispatch(fetchUser('u2kjfladadwddj9'))
+    store.dispatch(fetchUser("u2kjfladadwddj9"));
   }
   render() {
     return (
       <Router>
         <Provider store={store}>
           <div className="App">
-            <header className="App-header text-red">
-              <p>
-                Edit <code>src/App.js</code> and save to reload.
-              </p>
+            <header className="App-header text-red p-4">
+              <Popup
+                trigger={<button className="button"> Right Top </button>}
+                position="bottom left"
+                on="hover"
+              >
+                <div>We here yo!</div>
+              </Popup>
+
               <a
                 className="App-link"
                 href="https://reactjs.org"
