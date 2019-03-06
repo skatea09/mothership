3e4b4374689173786f3d4994308c5b8d6b066fff
Ammon <skatea09@aol.com>
2019-03-06 02:02:38 -0800

    removes purgescss who needs it

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,7 @@
-const tailwindcss = require("tailwindcss");
-const purgecss = require('@fullhuman/postcss-purgecss');
-
+const tailwindcss = require('tailwindcss');
 module.exports = {
   plugins: [
-    tailwindcss("./tailwind.config.js"),
-    require("autoprefixer"),
-    process.env.NODE_ENV === "production" &&
-      purgecss({
-        content: ["./src/**/*.js"],
-        css: ["./src/**/*.css"]
-      })
+    tailwindcss('./tailwind.config.js'),
+    require('autoprefixer')
   ]
-};
+}
